bd887b80978a00becba4964ac0e97cb236ef55e2
wnuklipm <marcin.wnuk_lipinski@contractors.roche.com>
2021-07-26 10:51:27 +0200

    Add flyway dependency and sql script.

---
+++ src/main/resources/database/init.sql
@@ -0,0 +1,3 @@
+INSERT INTO COURSE (DESCRIPTION, TITLE, URL_TO_COVER) VALUES ('Easy course...', 'MMA for beginners', '/mma-beginners.png');
+INSERT INTO COURSE (DESCRIPTION, TITLE, URL_TO_COVER) VALUES ('Medium course...', 'MMA for medium', '/mma-medium.png');
+INSERT INTO COURSE (DESCRIPTION, TITLE, URL_TO_COVER) VALUES ('Hard course...', 'MMA for hard', '/mma-hard.png');
